46966d4a84b24e9ce5d55604c6585a4946163604
Steve Bresnick <49722486+pantheonsteve@users.noreply.github.com>
2021-11-02 15:09:01 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ArticleList from './Components/Article/ArticleList';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+const LIST_URL = 'https://dev-nasdaq-feeds-demo.pantheonsite.io/jsonapi/node/article';
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {data: null };
+    this.loadArticles = this.loadArticles.bind(this);
+    this.updateData = this.updateData.bind(this);
+  }
+
+  loadArticles() {
+    //fetch Articles.
+    fetch(LIST_URL, {mode:'cors'}).then(function (response) {
+      return response.json();
+    })
+    .then((data) => this.updateData(data))
+    .catch(err => console.log('Fetching Articles Failed', err));
+  }
+
+  updateData(responseData) {
+   this.setState({data: responseData.data});
+ }
+
+  componentWillMount() {
+    this.loadArticles();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+        <ArticleList
+          data={this.state.data}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
